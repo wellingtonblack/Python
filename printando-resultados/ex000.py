843d332c147a00e4ecec2d7f7e1781a7a6a94508
Wellington Silva <well.silvacp83@gmail.com>
2018-02-14 19:35:19 -0200

    tipos primitivos

--- printando-resultados/ex000.py
+++ printando-resultados/ex000.py
@@ -7,5 +7,4 @@ acompanhantes = input("quem costuma ser seus acompanhantes? ")
 sons = input("quantos filhos você tem? ")
 idade = input("qual a sua idade? ")
 
-print("Que maravilha você adora viajar ", ferias,"nas férias, e no inverno prefere ", inverno,"sensacional!!! sempre estando junto com",
-      acompanhantes,"pra completar voce tem", sons, "filho(a)(s), nos seus ",idade, "anos" )
+print(ferias, inverno, acompanhantes, sons, idade)
--- pythonteste/desafio04.py
+++ pythonteste/desafio04.py
@@ -1,6 +1,12 @@
-var = input("digite numeros ")
-print(var.isnumeric())
-var2 = input("digite letras ")
-print(var2.isalpha())
-var2 = input("digite espaços ")
-print(var2.isspace())
+var = input("digite algo ")
+tipo = type(var)
+space = var.isspace()
+num = var.isnumeric()
+alf = var.isalpha()
+alfnum = var.isalnum()
+maius  = var.isupper()
+minus = var.islower()
+cap = var.istitle()
+
+print("digite algo: {} \nO tipo primitivo é: {} \nTem espaços? {} \nÈ numérico? {} \nÉ alfabético? {} \nÉ alfanumérico? {} \nEstá em maiúsculas? {} \nEstá em minusculas? {} \nEsta capitalizada?{} "
+      .format(var, tipo, space, num, alf, alfnum, maius, minus, cap))
